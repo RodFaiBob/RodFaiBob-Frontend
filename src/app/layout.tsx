76037c91dd28bd698e4c4534fcced79543998905
montagan <bitoey2016@gmail.com>
2025-03-05 16:25:06 +0700

    Refactor global styles and update font imports for improved typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
+import { IBM_Plex_Sans_Thai } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"], // Choose the required weights
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const ibmPlexSansThai = IBM_Plex_Sans_Thai({
+  variable: "--font-ibm-plex-thai",
+  subsets: ["thai"],
+  weight: ["300", "400", "500", "600", "700"], // Choose the required weights
 });
 
 export const metadata: Metadata = {
@@ -25,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.variable} ${ibmPlexSansThai.variable}  antialiased`}
       >
         {children}
       </body>
