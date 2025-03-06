1507347b599d350b340ca2682e10de7ce71f7ce9
PKFO <perawat246@gmail.com>
2025-03-07 00:06:08 +0700

    update map homepage

--- src/modules/home/homepage.tsx
+++ src/modules/home/homepage.tsx
@@ -3,6 +3,7 @@ import Dropdown from '@/components/common/dropdown'
 import Image from 'next/image'
 import React from 'react'
 import BackgroundRodFai from '@/assets/home/BackgroundRodFai.svg'
+import TrainMap from '@/../public/trainmap.svg'
 
 const mockData = [
   {
@@ -17,8 +18,11 @@ const Homepage = () => {
   }
   return (
     <div className='min-h-screen w-full'>
-      <div className={`w-full h-30 flex items-center`} style={{backgroundImage: BackgroundRodFai}}>
-          {/* <Image src={BackgroundRodFai} alt='background' width={0} height={422} style={{ width: "100%" }} /> */}
+      <div className={`w-full h-100 flex items-center`} style={{ backgroundImage: `url(${BackgroundRodFai.src})` }}>
+          
+      </div>
+      <div className='w-full h-[1100px] flex items-center justify-center'>
+          <Image src={TrainMap} alt=''/>
       </div>
       {/* <Dropdown options={mockData} handleOnSelect={handleOnSelect}/> */}
     </div>
