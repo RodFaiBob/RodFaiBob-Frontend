8dbbe2cf1d4d8d4015386f2d44e28b6856f2e784
Vorachot Banluepongpan <vorachot.ban@gmail.com>
2025-03-07 16:52:13 +0700

    Add ellipse SVG asset and update VisualizePage with station and interchange components; enhance modal styling and functionality

--- src/modules/visualize/visualizepage.tsx
+++ src/modules/visualize/visualizepage.tsx
@@ -2,53 +2,146 @@
 import React from "react";
 import Image from "next/image";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const Modal = ({ onClose }: { title: string; onClose: () => void }) => {
+const InterchangePoint = ({
+  stnName,
+  stnCode,
+  colorCode,
+}: {
+  stnName: string;
+  stnCode: string;
+  colorCode: string;
+}) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex flex-col items-center gap-0.5">
+        <div
+          className={`w-6 h-6 rounded-full`}
+          style={{ backgroundColor: colorCode }}
+        ></div>
+        <div className="text-xs">{stnCode}</div>
+      </div>
+
+      <div className="font-thai ">{stnName}</div>
+    </div>
+  );
+};
+
+const StationPoint = ({
+  stnName,
+  stnCode,
+  colorCode,
+}: {
+  stnName: string;
+  stnCode: string;
+  colorCode: string;
+}) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div
+        className={`flex justify-center items-center w-10 h-10 border-3 rounded-full m-1`}
+        style={{ borderColor: colorCode }}
+      >
+        <div className="font-semibold">{stnCode}</div>
+      </div>
+      <div className="font-thai">{stnName}</div>
+    </div>
+  );
+};
+
+const Modal = ({ onClose }: { onClose: () => void }) => {
   return (
     <div className="absolute inset-0 bg-[#2D2C38]/25 flex justify-center items-center">
       <div className="bg-[#708C82] text-white rounded-xl w-[90%]">
         <div className="p-4">
           <div className="flex justify-between items-center">
-            <h2 className="text-2xl font-bold">Route and Fares</h2>
-            <button onClick={onClose} className="text-white text-xl">
+            <h2 className="text-2xl font-bold ml-10 mt-3">Route and Fares</h2>
+            <button
+              onClick={onClose}
+              className="cursor-pointer text-white text-xl"
+            >
               ✕
             </button>
           </div>
         </div>
 
-        <div className="m-1 bg-white p-4 rounded-sm ">
-          <p className="text-black text-sm">
-            <div className="flex justify-around">
-              <div>
-                <div className="flex gap-3">
+        <div className="m-1 bg-white p-4 rounded-b-xl ">
+          <div className="text-black text-sm">
+            <div className="flex items-start">
+              <div className="flex flex-col w-1/2 gap-2 border-r-2 border-[#708C82] m-2 pr-3">
+                <div className="flex gap-3 ml-3">
                   <Image
                     src="/assets/pin.svg"
                     width={20}
                     height={20}
                     alt="pin"
                   />
-                  <div>Origin</div>
+                  <div className="text-[#CDCDCD]">Origin</div>
                 </div>
-                <div>พิพิธภัณฑ์กองทัพอากาศ</div>
-                <div>O</div>
-                <div>O</div>
-                <div className="flex gap-3">
+                <div>
+                  <StationPoint
+                    stnName="พิพิธภัณฑ์กองทัพอากาศ"
+                    stnCode="N24"
+                    colorCode="#8CB63C"
+                  />
+                </div>
+
+                <div className="flex flex-col gap-2 ml-5 my-1">
+                  <div>
+                    <Image
+                      src="/assets/elipse.svg"
+                      width={8}
+                      height={8}
+                      alt="elipse"
+                    />
+                  </div>
+                  <div>
+                    <Image
+                      src="/assets/elipse.svg"
+                      width={8}
+                      height={8}
+                      alt="elipse"
+                    />
+                  </div>
+                </div>
+
+                <div className="flex gap-3 ml-3">
                   <Image
                     src="/assets/pin.svg"
                     width={20}
                     height={20}
                     alt="pin"
                   />
-                  <div>Destination</div>
+                  <div className="text-[#CDCDCD]">Destination</div>
+                </div>
+                <div>
+                  <StationPoint
+                    stnName="โชคชัย 4"
+                    stnCode="YL03"
+                    colorCode="#F5D411"
+                  />
                 </div>
               </div>
-              <div className="w-0.5 bg-[#708C82]"></div>
-              <div>Interchange</div>
+              <div className="flex flex-col ml-3 mt-2  gap-2">
+                <div className="font-semibold text-lg">Interchange</div>
+                <div className="flex items-center gap-2">
+                  <InterchangePoint
+                    stnName="ห้าแยกลาดพร้าว"
+                    stnCode="N09"
+                    colorCode="#8CB63C"
+                  />
+                  <div>→</div>
+                  <InterchangePoint
+                    stnName="พหลโยธิน"
+                    stnCode="BL14"
+                    colorCode="#2C347D"
+                  />
+                </div>
+              </div>
             </div>
-          </p>
-          <button
-            className="w-full bg-[#F5C2C2] font-semibold text-xl py-2 rounded-2xl mt-2"
-          >
+          </div>
+          <button className="w-full bg-[#F5C2C2] font-semibold text-xl py-2 rounded-2xl mt-2">
             Fare : 45 Bath
           </button>
         </div>
@@ -65,14 +158,14 @@ const SearchCard = ({ title }: { title: string }) => {
         {title}
       </h2>
       <div className="w-full h-100 bg-[#F1F0FF] rounded-lg mt-4">
-        {isOpen && <Modal title={title} onClose={() => setIsOpen(false)} />}
+        {isOpen && <Modal onClose={() => setIsOpen(false)} />}
       </div>
       <div className="flex justify-evenly mt-4 text-sm">
         <div className="font-semibold text-xl">Runtime : 1234</div>
         <div className="font-semibold text-xl">Memory Usage : 1234</div>
       </div>
       <button
-        className="w-full bg-[#708C82] text-white font-semibold text-2xl py-3 rounded-xl mt-4"
+        className="w-full cursor-pointer bg-[#708C82] text-white font-semibold text-2xl py-3 rounded-xl mt-4"
         onClick={() => {
           setIsOpen(!isOpen);
         }}
@@ -84,10 +177,14 @@ const SearchCard = ({ title }: { title: string }) => {
 };
 
 const VisualizePage = () => {
+  const router = useRouter();
   return (
-    <div className="min-h-screen flex flex-col font-pop bg-white">
+    <div className="min-h-screen flex flex-col bg-white">
       <div className="w-full bg-[#F1F0FF]  relative">
-        <button className="flex gap-1 absolute left-4 top-10 transform -translate-y-1/2 text-2xl font-bold">
+        <button
+          className="flex cursor-pointer gap-1 absolute left-4 top-10 transform -translate-y-1/2 text-2xl font-bold"
+          onClick={() => router.replace('/')}
+        >
           <Image src="/assets/back.svg" width={30} height={30} alt="back" />
           Back
         </button>
@@ -103,7 +200,7 @@ const VisualizePage = () => {
         <SearchCard title="Heuristic Search" />
       </div>
       <div className="w-full flex justify-center">
-        <button className="w-90 flex justify-center gap-5 bg-[#F5C2C2] text-white font-semibold text-2xl py-3 rounded-2xl mt-4 mb-10">
+        <button className="w-90 cursor-pointer flex justify-center gap-5 bg-[#F5C2C2] text-white font-semibold text-2xl py-3 rounded-2xl mt-4 mb-10">
           <Image src="/assets/start.svg" width={15} height={20} alt="start" />
           Start Animation
         </button>
