1fe4360b17fbc96597cf905ee98e33dce617a18b
mewpk <milzaza123@gmail.com>
2025-03-09 22:03:52 +0700

    Increase Axios timeout to 300 seconds for improved request handling

--- src/utils/Axios.ts
+++ src/utils/Axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: 'https://rodfaibob-backend.micromew.com', // Replace with your default domain
-    timeout: 100000,
+    timeout: 300000,
     headers: { 'Content-Type': 'application/json' }
 });
 
